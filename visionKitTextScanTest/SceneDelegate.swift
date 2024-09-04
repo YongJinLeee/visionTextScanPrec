02eeba6c693a3d025037399168b02b6fe3fc9ece
YongJinLeee <YJayLee.Dev@gmail.com>
2024-09-04 14:59:30 +0900

    [Fixed] 내비컨트롤러 기반으로 변경

--- visionKitTextScanTest/SceneDelegate.swift
+++ visionKitTextScanTest/SceneDelegate.swift
@@ -14,10 +14,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = DefaultViewController()
+
+        window = UIWindow(windowScene: windowScene)
+        let defaultViewController = DefaultViewController()
+        let navigationController = UINavigationController(rootViewController: defaultViewController)
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
